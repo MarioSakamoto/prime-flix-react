712f9828c8faa9ca7a2d879ee902894ecf425c47
Mario Sakamoto <mariosakamoto.dev@gmail.com>
2022-11-14 01:01:33 -0300

    Loading do filme

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,61 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState} from 'react';
 import api from '../../services/api';
 import { Link } from 'react-router-dom';
 import './home.css';
 
+// URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
+
 function Home(){
-    const[filmes, setFilmes] = useState([]);
+  const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
 
-        async function loadFilmes(){
-            const response = await api.get("movie/now_playing", {
-                params:{
-                    api_key: "f156c329a0911c1549f0389581941b0d",
-                    language: "pt-BR",
-                    page:1,
-                }
-            })
+  useEffect(()=>{
 
-            //console.log(response.data.results.slice(0,10));
-            setFilmes(response.data.results.slice(0, 10))
-
+    async function loadFilmes(){
+      const response = await api.get("movie/now_playing", {
+        params:{
+         api_key: "28fc232cc001c31e8a031f419d0a14ca",
+         language: "pt-BR",
+         page: 1,
         }
+      })
 
-        loadFilmes();
-    }, [])
+      //console.log(response.data.results.slice(0, 10));
+      setFilmes(response.data.results.slice(0, 10))
+      setLoading(false);
 
+    }
+
+    loadFilmes();
+
+  }, [])
+
+
+
+  if(loading){
     return(
-        <div className="container">
-            <div className="lista-filmes">
-            {filmes.map((filmes) => {
-                return(
-                    <article key={filme.id}>
-                        <strong>{filmes.title}</strong>
-                        <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
-                        <Link to={`/filme/${filme.id}`}>Acessar</Link>
-                    </article>
-                )
-            })}
-           </div>
-        </div>
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
     )
+  }
+
+  return(
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Home;
