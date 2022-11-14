61b16f359f4fa6ab988fb6ace31f6cec7d6f2742
Mario Sakamoto <mariosakamoto.dev@gmail.com>
2022-11-13 23:14:17 -0300

    Criando nossa Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,27 @@
+import {useEffect, useState} from 'react';
+import api from '../../services/api';
 
 function Home(){
+    const[filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key: "f156c329a0911c1549f0389581941b0d",
+                    language: "pt-BR",
+                    page:1,
+                }
+            })
+
+            console.log(response.data.results);
+
+        }
+
+        loadFilmes();
+    }, [])
+
     return(
         <div>
             <h1>BEM VINDO A HOME</h1>
